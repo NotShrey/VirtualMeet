59432a3707d30754008d3822fcd0f22493e2cf34
NotShrey <shreyas.nayak.003@gmail.com>
2024-06-05 03:16:56 +0530

    Implemented Navbar, signUp(auth)

---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Recording = () => {
+  return (
+    <section className='flex size-full flex-col gap-10 text-white'>
+    <h1 className='text-3xl font-bold'>Recordings are here</h1>
+    
+  </section>
+  )
+}
+
+export default Recording
